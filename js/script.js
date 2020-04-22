cdc58057f971ea829b14c03eaa22f705441e7521
Amanda Morris <amandahenry2@comcast.net>
2020-04-22 08:19:18 -0600

    will work on timer and appending next

--- js/script.js
+++ js/script.js
@@ -19,16 +19,12 @@ var trivia =
   answers:['Bassian thrush','Budgie, or common parakeet', 'Penguins', 'Unkindness', 'Cormorant' ],
 }
 
-
 startBtn.addEventListener("click", function startGame() {
 renderQuestion();
 });
 
 
-
 //need to create an objarray to store, question options, answer?
-
-
 
 
 //need a function to display the card
@@ -40,38 +36,40 @@ function renderQuestion(){
   document.querySelector("#start").style.display= "none";
   console.log("renderQuestion");
   //need to check to see at the end or not
-  if(questionIndex< trivia.questions.length)
+  if(questionIndex < trivia.questions.length)
   {
   //if we are at the encodeURI//display end
 
   //trivia object
 console.log(trivia)
+
+
 //question
 console.log(trivia.questions[questionIndex])
 
 document.querySelector("#questions").textContent = (questionIndex+1)+ ". "+trivia.questions[questionIndex];
+
+
 //choices
 // console.log(trivia.choices[questionIndex][0])
+
 //clear out choices area to prevent overappending
 document.querySelector("#choices").textContent ="";
 
 for(var i=0;i<trivia.choices[questionIndex].length;i++){
   console.log(trivia.choices[questionIndex][i]);
-  var choicebtn= document.createElement("button");
-  //choicebtn = <button></button>
-  choicebtn.setAttribute("class", "choice");
-  //choicebtn = <button class="choice"></button>
-  choicebtn.setAttribute("value",trivia.choices[questionIndex][i] );
-   //choicebtn = <button class="choice" value="hummingbird"></button>
-   choicebtn.textContent= trivia.choices[questionIndex][i];
-   //choicebtn = <button class="choice" value="hummingbird">hummingbird</button>
+  var choiceBtn= document.createElement("button");
+  //choiceBtn = <button></button>
+  choiceBtn.setAttribute("class", "choice");
+  //choiceBtn = <button class="choice"></button>
+  choiceBtn.setAttribute("value",trivia.choices[questionIndex][i] );
+   //choiceBtn = <button class="choice" value="hummingbird"></button>
+   choiceBtn.textContent= trivia.choices[questionIndex][i];
+   //choiceBtn = <button class="choice" value="hummingbird">hummingbird</button>
+   //when user onclicks, run the checkQuestion function
+   choiceBtn.onclick=checkQuestion;
    
-   //when user onclicks you run the checkQuestion function
-   choicebtn.onclick=checkQuestion;
-   
-   document.querySelector("#choices").appendChild(choicebtn);
-
-
+   document.querySelector("#choices").appendChild(choiceBtn);
   //need to add data to btn
 
 }
@@ -108,13 +106,13 @@ console.log("GAME OVER");
         if(this.value == trivia.answers[questionIndex])
         {
           score++; //score=score+1
-          //display score update
+          //display score update in console
           document.querySelector("#score").textContent ="Score: "+ score;
         }
         else{
           //what happens wrong
         //reduce time by 10sec
-        console.log("wrong ans!!");
+        console.log("Incorrect!!");
         }
 
         //update the questionIndex
@@ -126,10 +124,10 @@ console.log("GAME OVER");
       }
        
 
-//function to check when you are done, or you can add this check before rendering card
+//function to check when you are done, or can add this check before rendering card
 //3. how do i know when i am done?
-    //if time is up
-    //if you are done answering 5 questions / done answering questions
+    //if time is up, or
+    //if you are done answering 5 questions
 
 //make a function to display when game is over.. and does the following below
 //4. what do i do when the game is over
@@ -144,29 +142,32 @@ console.log("GAME OVER");
 // Set the body to a variable
 var body = document.body;
 
-// Create all necessary body elements by declaring variables
+// Create additional body elements by declaring variables
+var headerEl = document.createElement("header");
 var h1El = document.createElement("h1");
 var h3El = document.createElement("h3");
-var infoEl = document.createElement("div");
-// var imgEl = document.createElement("img");
-var startBtn = document.createElement("button");
 
 
-var q1El = document.createElement("div");
-var listEl = document.createElement("ol");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
+// var infoEl = document.createElement("div");
+// // var imgEl = document.createElement("img");
+// startBtn = document.createElement("button");
+// startBtn.id="start";
 
-var q2El = document.createElement("div");
+
+// var q1El = document.createElement("div");
+// var listEl = document.createElement("ol");
+// var li1 = document.createElement("li");
+// var li2 = document.createElement("li");
+// var li3 = document.createElement("li");
+// var li4 = document.createElement("li");
+
+// var q2El = document.createElement("div");
 var list2El = document.createElement("ol");
 var li21 = document.createElement("li");
 var li22 = document.createElement("li");
 var li23 = document.createElement("li");
 var li24 = document.createElement("li");
 
-//will need to set up 3 more questions with 4 potential answers
 
 // Store our li elements in a variable.  Will need to do for 5 questions
 var listItems = document.getElementsByTagName("li");
@@ -175,83 +176,98 @@ var listItemsTwo = document.getElementsByTagName("li");
 // var listItemsFour
 // var listItemsFive
 
-
+// TIMER
 
 // //set up timer variables
-// var secondsDisplay = document.querySelector("#seconds");
+
+var startTimer = document.querySelector("#timer");
+var secondsDisplay = document.querySelector("#seconds");
 
 // //zero out the seconds, though maybe need to start the totalSeconds at 60?
-// var totalSeconds = 0;
-// var secondsElapsed = 0;
-// var interval;
-// var secondsLeft = (totalSeconds - secondsElapsed);
+var totalSeconds = 60;
+var secondsElapsed = 0;
+var interval;
+var secondsLeft = (totalSeconds - secondsElapsed);
 
 // // This function is where the timer runs
 // // This increments the secondsElapsed var
 // function startTimer() {
-//     setTime();
-//     //    
-//       /* the "interval" variable here using "setInterval()" begins the recurring increment of the 
-//          secondsElapsed variable which is used to check if the time is up */
-//         interval = setInterval(function() {
-//           secondsElapsed++;
-//           //So renderTime() is called here once every second.
-//           renderTime();
-//         }, 1000);
-//     }
+    // setTime();
+    //    
+      /* the "interval" variable here using "setInterval()" begins the recurring increment of the 
+         secondsElapsed variable which is used to check if the time is up */
+        interval = setInterval(function() {
+          secondsElapsed++;
+          //So renderTime() is called here once every second.
+          // renderTime();
+        }, 1000);
+    
+
+    console.log(totalSeconds)
 
 //Need to set up the conditional if/else for the timer if wrong answer and subtract 10 seconds
+
+startBtn.addEventListener("click", startTimer);
+
+
+
+
 
 // Set the text content of relevant elements
 h1El.textContent = "Bird Quiz Challenge";
 h3El.textContent = "Try to answer the following bird-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
-buttonEl.textContent = "Start Quiz";
+startBtn.textContent = "Start Quiz";
 
-q1El.textContent = "What worm eater finds its food by directing its farts toward dirt?";
-li1.textContent = "Albatross";
-li2.textContent = "Passerine";
-li3.textContent = "Bassian thrush";
-li4.textContent = "Swift";
+// q1El.textContent = "What worm eater finds its food by directing its farts toward dirt?";
+// li1.textContent = "Albatross";
+// li2.textContent = "Passerine";
+// li3.textContent = "Bassian thrush";
+// li4.textContent = "Swift";
 
-q2El.textContent = "What species of birds is susceptible to contagious yawning?";
-li21.textContent = "The budgie, or common parakeet";
-li22.textContent = "Passerine";
-li23.textContent = "Bassian thrush";
-li24.textContent = "Swift";
+// q2El.textContent = "What species of birds is susceptible to contagious yawning?";
+// li21.textContent = "The budgie, or common parakeet";
+// li22.textContent = "Passerine";
+// li23.textContent = "Bassian thrush";
+// li24.textContent = "Swift";
 
 // Add 3 more questions with 4 potential answers
 
-
 // Append all of our elements
-body.appendChild(h1El);
-body.appendChild(h3El);
-body.appendChild(infoEl);
+
+body.appendChild(headerEl);
+headerEl.appendChild(h1El);
+headerEl.appendChild(h3El);
+
+body.appendChild(div);
+div.appendChild(startBtn);
+// body.appendChild(infoEl);
 // infoEl.appendChild(imgEl);
-infoEl.appendChild(buttonEl);
-body.appendChild(q1El);
-body.appendChild(q2El);
+// infoEl.appendChild(startBtn);
+// body.appendChild(q1El);
+// body.appendChild(q2El);
 
-q1El.appendChild(listEl);
-listEl.appendChild(li1);
-listEl.appendChild(li2);
-listEl.appendChild(li3);
-listEl.appendChild(li4);
+// q1El.appendChild(listEl);
+// listEl.appendChild(li1);
+// listEl.appendChild(li2);
+// listEl.appendChild(li3);
+// listEl.appendChild(li4);
 
-q2El.appendChild(list2El);
-list2El.appendChild(li21);
-list2El.appendChild(li22);
-list2El.appendChild(li23);
-list2El.appendChild(li24);
+// q2El.appendChild(list2El);
+// list2El.appendChild(li21);
+// list2El.appendChild(li22);
+// list2El.appendChild(li23);
+// list2El.appendChild(li24);
+
 
 
 // Style all of our elements
 h1El.setAttribute("style", "margin:auto; width:50%; text-align:center; font-family:arial; padding:20px;");
 h3El.setAttribute("style", "margin:auto; width:50%; text-align:center; font-family:arial; padding:10px;");
-infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center; font-family:arial;");
+// infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center; font-family:arial;");
 // imgEl.setAttribute("src", "assets/gulls_200_250.jpg");
 // imgEl.setAttribute("height", 250);
 // imgEl.setAttribute("width", 200);
-buttonEl.setAttribute("style", "font-size:25px; text-align:center; font-family:arial;");
+startBtn.setAttribute("style", "font-size:25px; text-align:center; font-family:arial;");
 
 
 q1El.setAttribute("style", "font-size:20px; font-family:arial;");
@@ -269,8 +285,6 @@ listItemsTwo[2].setAttribute("style", "color:white; background: #888888; padding
 listItemsTwo[3].setAttribute("style", "color:white; background: #999999; padding: 5px; margin-left: 35px;");
 
 
-//Need to set up eventListener onClick for button
 
-startBtn.addEventListener("click", startTimer);
 
 //Need to set up initials and score that is stored in global memory and a way for user to input with a prompt
